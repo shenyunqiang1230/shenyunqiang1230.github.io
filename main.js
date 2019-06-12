ef20edd0792dbe077e4735e93e3e3d327a7937ff
shenyunqiang1230 <1904069418@qq.com>
2019-06-12 19:21:10 +0800

    亚里莎

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log('hello world')
+var lovelyGirl=(function(){
+ var name='鸭梨傻';
+ var age =18;
+ function BeautifulGirl (){
+     this.race='sprite';
+ } 
+var like=new BeautifulGirl();
+return {
+    herName:name,
+    herAge=age
+}
+}());
+console.log(lovelyGril)
